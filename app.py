8fc554e04a2af8f9ef09e268fb78a549496d2b67
[mad-marvin] <[marvin.willms00@gmail.com]>
2019-11-12 20:49:16 +0100

    added database

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
 
+    database_contents = ''
+
+    conn = sqlite3.connect('sqlite/library.db')
+    try:
+        c = conn.cursor()
+        rows = c.execute('SELECT * FROM book_book')
+        for row in rows:
+            database_contents += f"<p>{row}</p>\n"
+        return '<h1>Database Content test:</h1>' + database_contents
+    finally:
+        conn.close()
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
 
